2cbc4a407eb00980b39e1f5699926db742f86e39
Bogdan <bgdnchrkschnk@gmail.com>
2022-07-17 16:40:47 +0300

    lesson 3

--- lesson 3.py
+++ lesson 3.py
@@ -1,7 +1,59 @@
-value = int(input('Ваш возраст:'))
-if value > 18:
-    print('Доступ к 18+ контенту открыт! Вам', value, 'лет.')
-elif value == 18:
-    print('Доступ к 18+ контенту открыт! Вам', value, 'лет.')
-else:
-    print('Доступ к 18+ контенту закрыт! Вам', value, 'лет!')
+# value = int(input('Ваш возраст:'))
+# if value > 18:
+#     print('Доступ к 18+ контенту открыт! Вам', value, 'лет.')
+# elif value == 18:
+#     print('Доступ к 18+ контенту открыт! Вам', value, 'лет.')
+# else:
+#     print('Доступ к 18+ контенту закрыт! Вам', value, 'лет!')
+
+# value = int(input("Число:"))
+# if value % 3 == 0 and value % 2 == 0:
+#     print('Делится на 6')
+# elif value % 2 == 0:
+#     print('Делится на 2')
+# elif value % 3 == 0:
+#     print('Делится на 3')
+# else:
+#     print('Не делится')
+
+
+# value = int(input("Число:"))
+# if not value % 3 and not value % 2:
+#     print('Делится на 6')
+# elif not value % 2:
+#     print('Делится на 2')
+# elif not value % 3:
+#     print('Делится на 3')
+# else:
+#     print('Не делится')
+
+
+#
+# number = int(input('Твое число:'))
+# result = 'Нечётное' if (number % 2) else 'Чётное'
+# print(result)
+
+
+# number = int(input('Твое число:'))
+# if number % 2 != 0:
+#     result = number, "нечетное число"
+# else:
+#     result = number, 'четное число'
+# print(result)
+
+
+
+# Форматируемые строки
+
+# value = 15
+# result = (“Мне”, value, “лет”)
+# print (result)
+#
+# преобразуем в
+#
+# value = 15
+# result = f"Мне {value} лет!"
+# print (result)
+
+
+
