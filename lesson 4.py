cd938084fa45989b969d71f36fecb692b350a44a
Bogdan <bgdnchrkschnk@gmail.com>
2022-07-20 23:35:35 +0300

    lesson 3

---
+++ lesson 4.py
@@ -0,0 +1,52 @@
+# my_str = '''"Русский военный корабль, иди нахуй!'''
+# print(my_str.find('а')) - поиск первой ‘a’ слева направо
+
+# my_str = '''"Русский военный корабль, иди нахуй!'''
+# print(my_str.rfind('а')) - поиск первой ‘a’ справа налево
+
+# my_str = '''"Русский военный корабль, иди нахуй!'''
+# new_str = my_str.replace('нахуй', 'к чёрту')
+# print (new_str)
+
+
+# value = input('input here:')
+# print (value.capitalize())
+
+
+# value = input('input here:')
+# print (value.count('e'))
+
+
+# value = 'uhUwbv4icn'
+# print(value.upper().count('IC'))
+
+# vlist = [1,2,3.15, 'asd']
+# vkortezh = (1,2,3.15, 'asd')
+# vlist = tuple(vlist)
+# vkortezh = list(vkortezh)
+# print(vlist, type(vlist))
+# print(vkortezh, type(vkortezh))
+#
+# print(vkortezh[0:4])
+
+# my_t = (1, 2, 3.15, "asd")
+# index = 3
+# new_t = my_t[index]
+# print (new_t)
+
+
+# my_l = [1, 2, 3.15, "asd"]
+# print (my_l)
+# my_l[0] = "test"
+# print(my_l)
+
+# my_l = [1, 2, 3.15, "asd"]
+# new_l = ([1,2,3], my_l)
+# print(new_l, type(new_l))
+# new_l[0][0] = "test"
+# print(new_l, type(new_l))
+
+list1 = [[1,2,5,'asd']]
+list2 = list1 * 30
+print(list2)
+
