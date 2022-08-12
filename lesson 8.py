765a4e66f3ba8f6b2df9a608e945f40ae5748761
Bogdan <bgdnchrkschnk@gmail.com>
2022-08-12 16:34:23 +0300

    Some adds

---
+++ lesson 8.py
@@ -0,0 +1,14 @@
+# mydict = {"name":"Bogdan", "surname":"Cherkaschenko", "age" : 26}
+# mydict["address"] = "prospkt Shevchenko"
+# print(mydict)
+
+# val = list("asd")
+# digits = list("12345678")
+# students = ["Cherkaschenko", "Shpanchuk", "Okhrimenko", "Murovanuy", "Urgaliev", "Dzhavahishvili"]
+# print(dict(zip(digits,students)))
+# print(list(zip(students,digits,val)))
+
+
+keys = ["a", "b", "c"]
+dic = {status:200 for status in keys}
+print(dic)
