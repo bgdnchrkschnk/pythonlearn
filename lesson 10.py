7e018f4fb8e2016f92fd60eb960569040d06eb1e
Bogdan <bgdnchrkschnk@gmail.com>
2022-08-26 00:41:36 +0300

    Some adds

---
+++ lesson 10.py
@@ -0,0 +1,41 @@
+import os
+dirr = "chlen"
+# print(os.listdir(dirr))
+#
+# dirr2 = "chlen/chlenix"
+# print(os.listdir(dirr2))
+#
+# curfilepath = os.getcwd()
+# print(curfilepath)
+
+
+# print(os.path.join(dirr, 'chlenix'))
+
+# listfiles = []
+# for file in os.listdir(os.getcwd()):
+#     if os.path.isfile(file):
+#         listfiles.append(file)
+# print(listfiles)
+
+# dirfiles = "chlen"
+# listfiles = []
+# for file in os.listdir(dirfiles):
+#     if os.path.isfile(os.path.join(dirfiles, file)):
+#         listfiles.append(file)
+# print(listfiles)
+
+
+# try:
+#     os.mkdir("MILAN STAR - Ne takaya")
+# except FileExistsError:
+#     print("Path already created!")
+
+
+# os.makedirs("MILAN STAR/POLNOLUNIE", exist_ok=True)
+
+
+# for file in os.listdir("chlen/chlenix"):
+#     if os.path.isfile(os.path.join("chlen/chlenix", file)):
+#         print(file)
+#     else:
+#         print("no")
