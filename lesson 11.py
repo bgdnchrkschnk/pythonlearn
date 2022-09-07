5c808bdf942e88c6af18a50c1e54326268da5e74
Bogdan <bgdnchrkschnk@gmail.com>
2022-09-08 00:52:33 +0300

    Some adds

--- lesson 11.py
+++ lesson 11.py
@@ -1,11 +1,51 @@
 import csv
 
-path = "test.csv"
-def read_csv(path):
-    with open(path, "r") as f:
+# path = "test.csv"
+# def read_csv(path):
+#     with open(path, "r") as f:
+#         data = []
+#         file_csv = csv.reader(f)
+#         for row in file_csv:
+#          data.append(row)
+#     print(data)
+# read_csv(path)
+import random
+
+
+# def read_csv(path):
+#     data = []
+#     with open (path, 'r') as f:
+#         file = csv.reader(f)
+#         for row in file:
+#             data.append(row)
+#     return data
+#
+# data = read_csv("persons.csv")
+
+# for row in data[1:]:
+#     row[1] = int(row[1]) + 100
+#
+# data[0].append("Education")
+# for row in data[1:]:
+#     row.append(random.choice([0,1]))
+
+def read_dict_csv(path):
+    with open (path, 'r') as f:
         data = []
-        file_csv = csv.reader(f)
-        for row in file_csv:
-         data.append(row)
-    print(data)
-read_csv(path)
+        file = csv.DictReader(f)
+        for row in file:
+            data.append(dict(row))
+    return data
+data = read_dict_csv("persons.csv")
+for row in data:
+    row['Education'] = random.choice([0,1])
+print(data)
+
+# def writer_csv(path, data):
+#     with open (path, 'w') as f:
+#         file = csv.writer(f)
+#         file.writerows(data)
+#
+# read_csv("persons.csv")
+# writer_csv("persons_futuristic.csv", data)
+
