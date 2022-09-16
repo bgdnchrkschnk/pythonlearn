e9087279cbb1b16e637667a5aac3914aad42c201
Bogdan <bgdnchrkschnk@gmail.com>
2022-09-17 02:13:35 +0300

    Some adds

--- task 11.py
+++ task 11.py
@@ -44,12 +44,75 @@
 #
 # print(easy_unpack((2,4,3,5,4,3,2)))
 
-# from typing import iterable
-def remove_all_before(items: list, border: int):
-    for i, val in enumerate(items):
-        if val==border:
-            return items[i:]
-            break
-    return items
-print(remove_all_before([3,5,3,4,0,3,6,5],0))
+# # from typing import iterable
+# def remove_all_before(items: list, border: int):
+#     for i, val in enumerate(items):
+#         if val==border:
+#             return items[i:]
+#             break
+#     return items
+# print(remove_all_before([3,5,3,4,0,3,6,5],0))
+#
+# import string
+# def is_all_upper(text: str) -> bool:
+#     return text.upper() == text
+# print(is_all_upper("VVV"))
+
+# def replace_first(items: list):
+#     try:
+#         items.append(items.pop(0))
+#         return items
+#     except IndexError:
+#         return items
+#
+# print(replace_first([]))
+
+
+# def max_digit(a: int) -> int:
+#   return int(max(str(a)))
+#
+# print(max_digit(3267869509979000)
+
+
+# def beginning_zeros(a: str) -> int:
+#     counter = 0
+#     for v in a:
+#         if v == "0":
+#             counter += 1
+#         else:
+#             break
+#     return counter
+#
+# print(beginning_zeros("0000067248789"))
+
+# def between_markers(text: str, start: str, end:str) -> str:
+#     start_index = text.find(start)
+#     end_index = text.find(end)
+#     return text[start_index+1:end_index]
+#
+# print(between_markers('What is >apple<', '>', '<'))
+
+# def split_pairs(text: str):
+#     list = []
+#     for index, value in enumerate(text):
+#             if index % 2 == 0:
+#                 try:
+#                     list.append(text[index]+text[index+1])
+#                 except IndexError:
+#                     list.append(text[index] + "_")
+#     return list
+# print(split_pairs("eubiuhifwef"))
+
+def correct_sentence(text: str) -> str:
+    if not text[0].isupper():
+        text = text[0].upper() + text[1:]
+    if text[-1] != ".":
+        text += "."
+    return text
+
+print(correct_sentence("greetings, friends"))
+
+
+
+
 
