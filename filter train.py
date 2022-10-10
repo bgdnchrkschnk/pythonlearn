d9b4502a65c07dd34be2af39fad80301bf95fe6a
Bogdan <bgdnchrkschnk@gmail.com>
2022-10-10 14:57:26 +0300

    Some adds

---
+++ filter train.py
@@ -0,0 +1,13 @@
+l1 = [1,4,10,-15,-25,6,0,-1]
+
+
+def chotnie(i):
+    return not i%2
+
+def nechotnie(i):
+    return i%2
+
+l1_chotnie = list(filter(chotnie, l1))
+print(l1_chotnie)
+
+l1_nechotnie = list(filter(chotnie, l1))
