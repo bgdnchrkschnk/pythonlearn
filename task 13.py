4e37bf22bb7e1095680a4804a881f05628c55ab9
Bogdan <bgdnchrkschnk@gmail.com>
2022-10-20 00:39:10 +0300

    Some adds

--- task 13.py
+++ task 13.py
@@ -3,6 +3,7 @@
 
 import requests
 import time
+import csv
 def get_quotes(number):
     param = {"method": 'getQuote',
     "format": "json",
@@ -15,11 +16,9 @@ def get_quotes(number):
         author = data["quoteAuthor"]
         quote = data["quoteText"]
         if author and quote not in quotes:
-            quotes.append(quote)
+            quotes.append([author, quote])
             time.sleep(1)
     return quotes
-print(get_quotes(5))
-
 
 
 # 2. Написать функцию, которая принимает результат предыдущей функции и сохраняет в csv файл.
@@ -28,3 +27,14 @@ print(get_quotes(5))
 # Author, Quote, URL.
 # Перед сохранением в csv, записи отсортировать по автору (в алфавитном порядке).
 # Разделитель - запятая.
+
+
+def quote_csv(name):
+    quotes = get_quotes(5)
+    with open(name, "w") as f:
+        file = csv.writer(f)
+        file.writerow(["Author", "Quote", "URL"])
+        for quote in quotes:
+            file.writerow([quote[0], quote[1], "http://api.forismatic.com/api/1.0/"])
+    return file
+print(quote_csv("new_quotes.csv"))
