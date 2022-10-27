974cfa697a9089c3e904fb9d01638183d51a504c
bgdnchrkschnk <bgdnchrkschnk@gmail.com>
2022-10-27 23:45:44 +0300

    Some adds

--- OOP.py
+++ OOP.py
@@ -52,25 +52,90 @@
 #     def display_breed(instance):
 #         print(f"My breed is {instance.breed}")
 
-class Cat:
-    breed = "Britain"
-    def display_breed(instance):
-        print(f"My breed is {instance.breed}")
-    def show_name(instance):
-        if hasattr(instance, "name"):
-            print(f"My name is {instance.name}")
-        else:
-            print("Noname")
-    def set_attribute(instance, value):
-        instance.name = value
-        print("Name is set successfully!")
-
-kokos = Cat()
-kokos.display_breed()
-kokos.set_attribute("Kokos")
-kokos.show_name()
+# class Cat:
+#     breed = "Britain"
+#     def display_breed(instance):
+#         print(f"My breed is {instance.breed}")
+#
+#         def show_name(instance):
+#             if hasattr(instance, "name"):
+#                 print(f"My name is {instance.name}")
+#             else:
+#                 print("Noname")
+#
+#         def set_attribute(instance, value):
+#             instance.name = value
+#             print("Name is set successfully!")
+#
+#
+# kokos = Cat()
+# kokos.display_breed()
+# kokos.set_attribute("Kokos")
+# kokos.show_name()
+# print(f"My breed is {instance.breed}")
+#     def show_name(instance):
+#         if hasattr(instance, "name"):
+#             print(f"My name is {instance.name}")
+#         else:
+#             print("Noname")
+#     def set_attribute(instance, value):
+#         instance.name = value
+#         print("Name is set successfully!")
+#
+# kokos = Cat()
+# kokos.display_breed()
+# kokos.set_attribute("Kokos")
+# kokos.show_name()
 
 
 # alisa = Cat()
 # alisa.name = "Alisa"
 # alisa.show_name()
+
+# class Car:
+#     def __init__(self, model, year, price):
+#         self.model = model
+#         self.year = year
+#         self.price = price
+#
+# mercedes = Car("cla 180", 2017, "25000$")
+# print(mercedes.model)
+
+# class Point:
+#     def __init__(self, x=0, y=0):
+#         self.x = x
+#         self.y = y
+#
+#     def move_to(self, new_x, new_y):
+#         self.x = new_x
+#         self.y = new_y
+#
+#     def go_home(self):
+#         self.move_to(0,0)
+#
+#     def print_point(self):
+#         print(f"Точка с координатами {self.x},{self.y}")
+
+    # def __str__(self):
+    #     return
+
+
+# p1 = Point()
+# print(dict(p1))
+# p1.move_to(15,20)
+# p1.go_home()
+# print(p1.__dict__)
+# p1.print_point()
+
+class Friends:
+    def __init__(self, name, age, job):
+        self.name = name
+        self.age = age
+        self.job = job
+    # def __repr__(self):
+    #     return f"My friend - {self.name}"
+    def __str__(self):
+        return f"Close friend - {self.name}"
+
+fr1 = Friends("Vladik", 26, "3D Animator")
+
