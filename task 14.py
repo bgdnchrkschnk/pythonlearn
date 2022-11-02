666f403022165a7f11841519650d12ed0512dae0
bgdnchrkschnk <bgdnchrkschnk@gmail.com>
2022-11-03 00:04:44 +0200

    Some adds

--- task 14.py
+++ task 14.py
@@ -14,30 +14,30 @@
 # а date_modified - эта же дата, представленная в формате "dd/mm/yyyy" (d-день, m-месяц, y-год)
 # Например [{"date_original": "8th February 1828", "date_modified": 08/02/1828},  ...]
 
-class AutoGet:
-    def __init__(self, domains_path, names_path):
-        self.domains_path = domains_path
-        self.names_path = names_path
-        self.my_domains()
-        self.get_surnames()
-
-    def my_domains(self):
-        with open (self.domains_path, "r") as txt:
-            readlines = txt.readlines()
-        domains = []
-        for line in readlines:
-            domains.append(line[1:-1])
-        print(domains)
-
-    def get_surnames(self):
-        with open (self.names_path, "r") as txt:
-            surnames = []
-            new_surnames = []
-            readlines = txt.readlines()
-            for line in readlines:
-                surnames.append(line.split())
-            for val in surnames:
-                new_surnames.append(val[1])
-            print(new_surnames)
-
-num1 = AutoGet("domains.txt", "names.txt")
+# class AutoGet:
+#     def __init__(self, domains_path, names_path):
+#         self.domains_path = domains_path
+#         self.names_path = names_path
+#         self.my_domains()
+#         self.get_surnames()
+#
+#     def my_domains(self):
+#         with open (self.domains_path, "r") as txt:
+#             readlines = txt.readlines()
+#         domains = []
+#         for line in readlines:
+#             domains.append(line[1:-1])
+#         print(domains)
+#
+#     def get_surnames(self):
+#         with open (self.names_path, "r") as txt:
+#             surnames = []
+#             new_surnames = []
+#             readlines = txt.readlines()
+#             for line in readlines:
+#                 surnames.append(line.split())
+#             for val in surnames:
+#                 new_surnames.append(val[1])
+#             print(new_surnames)
+#
+# num1 = AutoGet("domains.txt", "names.txt")
