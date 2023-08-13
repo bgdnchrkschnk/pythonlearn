b373ebdf3a085367105b3da9558ec4e7062ff0d3
bgdnchrkschnk <bgdnchrkschnk@gmail.com>
2023-08-13 17:08:17 +0300

    Some adds

--- selenium_learning/actionchains.py
+++ selenium_learning/actionchains.py
@@ -39,33 +39,33 @@ actions.drag_and_drop(source=selenium_link, target=image_search_box).perform()
 
 sleep(3)
 
-# command_key = Keys.COMMAND if sys.platform == "darwin" else Keys.CONTROL
-#
-# actions.key_down(command_key)\
-#     .send_keys('a')\
-#     .key_up(command_key)\
-#     .key_down(command_key)\
-#     .send_keys('c')\
-#     .key_up(command_key)\
-#     .pause(5)\
-#     # .drag_and_drop(source=selenium_link, target=search_button) \
-#     .drag_and_drop_by_offset(source=selenium_link, xoffset=100, yoffset=100) \
-#     .perform()
+command_key = Keys.COMMAND if sys.platform == "darwin" else Keys.CONTROL
 
-# Actions can be queeued one by one
+actions.key_down(command_key)\
+    .send_keys('a')\
+    .key_up(command_key)\
+    .key_down(command_key)\
+    .send_keys('c')\
+    .key_up(command_key)\
+    .pause(5)\
+    # .drag_and_drop(source=selenium_link, target=search_button) \
+    .drag_and_drop_by_offset(source=selenium_link, xoffset=100, yoffset=100) \
+    .perform()
 
-# actions.click(on_element=search_field)
-# sleep(1)
-# actions.send_keys_to_element(search_field, "python + selenium")
-# sleep(1)
-# # actions.click(on_element=search_button)
-# actions.key_down(Keys.ENTER)
-# actions.key_up(Keys.ENTER)
-# actions.perform()
-# sleep(5)
-# selenium_link = driver.find_element(By.CSS_SELECTOR, ".b_topTitle>a")
-# sleep(3)
-# assert selenium_link.is_displayed()
-# assert selenium_link.is_enabled()
+Actions can be queeued one by one
+
+actions.click(on_element=search_field)
+sleep(1)
+actions.send_keys_to_element(search_field, "python + selenium")
+sleep(1)
+# actions.click(on_element=search_button)
+actions.key_down(Keys.ENTER)
+actions.key_up(Keys.ENTER)
+actions.perform()
+sleep(5)
+selenium_link = driver.find_element(By.CSS_SELECTOR, ".b_topTitle>a")
+sleep(3)
+assert selenium_link.is_displayed()
+assert selenium_link.is_enabled()
 
 driver.quit()
--- selenium_learning/chr.py
+++ selenium_learning/chr.py
@@ -1,7 +1,23 @@
-from selenium.webdriver import Chrome
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from selenium.webdriver import ActionChains, Keys
+from selenium.webdriver.common.by import By
+from selenium_learning.selenium_test_task.fixtures import *
+from time import sleep
 
-chrome_browser = Chrome(service=Service(ChromeDriverManager().install()))
 
-chrome_browser.quit()
+def test_page_is_correct(chrome_browser):
+    actions = ActionChains(chrome_browser)
+    chrome_browser.get("https://google.com")
+    sleep(2)
+    search_line = chrome_browser.find_element(By.CSS_SELECTOR, ".gLFyf")
+    search_line.send_keys("milan star")
+    actions\
+        .key_down(Keys.ENTER)\
+        .key_up(Keys.ENTER)\
+        .perform()
+    sleep(5)
+    link = chrome_browser.find_element(By.XPATH, "//h3[contains(text(), 'Тверк')]")
+    actions\
+        .scroll_to_element(element=link)\
+        .pause(5)\
+        .perform()
