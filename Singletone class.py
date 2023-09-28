519440a07e2253c9b8d11971b65a7f343a379f9a
bgdnchrkschnk <bgdnchrkschnk@gmail.com>
2023-09-29 01:56:34 +0300

    гнр

--- Singletone class.py
+++ Singletone class.py
@@ -1,15 +1,16 @@
 class MyPartner:
-    __INSTANCE = 0
+    __INSTANCE = None
 
     def __new__(cls, *args, **kwargs):
         if not cls.__INSTANCE:
             cls.__INSTANCE = super().__new__(cls)
-        return cls.__INSTANCE
+        else:
+            raise Exception("Instance already exists!")
 
     def __del__(self):
         self.__class__.__INSTANCE = None
 
-    def __init__(self, name, age, sex, **kwargs):
+    def __init__(self, name, age, sex):
         self.name = name
         self.age = age
         self.sex = sex
@@ -18,5 +19,5 @@ class MyPartner:
 
 a = MyPartner("Anya", 21, "Female")
 s = MyPartner("Sveta", 38, "Female")
-s.__del__()
-print(s)
+# s.__del__()
+print(a.age, s.age)
